e3ab6ed345e4cc0a9b0bfd016cdff6a350fdd9da
sergey730594 <sergey730594@gmail.com>
2026-08-23 20:08:27 +0400

    2026

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -107,7 +107,7 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({ isOpen, onClose, languag
         {/* Нижняя часть */}
         <div className="border-t border-gray-200 p-4">
           <p className="text-xs text-gray-500 text-center">
-            © 2024 Grant Bakery
+            © 2026 Grant Bakery
           </p>
         </div>
       </div>
