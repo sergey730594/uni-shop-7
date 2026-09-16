5c6d31122e251e176a0515a023e468116530c8ff
null <null>
2026-09-16 18:28:02 +0400

    resend v1

--- api/send-order.mjs
+++ api/send-order.mjs
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.re_VJU8MeuF_JvfWxiWWRNTmT6onmftcG1yj);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
