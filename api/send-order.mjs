c5267e83023e977af68e2232931c716d9ecf9d19
sergey730594 <sergey730594@gmail.com>
2026-09-16 19:13:11 +0400

    resend v11

--- api/send-order.mjs
+++ api/send-order.mjs
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.re_VJU8MeuF_JvfWxiWWRNTmT6onmftcG1yj);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
